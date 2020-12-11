342262b14c9090a6acdfe386656087cd9392db6f
Wei Zhu <yesmeck@gmail.com>
2020-12-11 22:52:27 +0800

    more test

--- lib/MethodDeclaration.test.ts
+++ lib/MethodDeclaration.test.ts
@@ -1,21 +1,31 @@
 import { InterfaceDeclaration } from './InterfaceDeclaration';
 import { MethodDeclaration } from './MethodDeclaration';
 
-const nsview = require('../documentation/appkit/nsview.json');
-const initWithFrame = require('../documentation/appkit/nsview/1483458-initwithframe.json');
 
 describe('MethodDeclaration', () => {
-  it('simple method', () => {
-    const interfaceDecl = InterfaceDeclaration.initFromTokens(
-      '"doc://com.apple.documentation/documentation/appkit/nsview"',
-      {},
-      nsview.primaryContentSections[0].declarations[0].tokens
-    );
+  const nsview = require('../documentation/appkit/nsview.json');
+  const interfaceDecl = InterfaceDeclaration.initFromTokens(
+    '"doc://com.apple.documentation/documentation/appkit/nsview"',
+    {},
+    nsview.primaryContentSections[0].declarations[0].tokens
+  );
 
+  it('simple method', () => {
+    const method = require('../documentation/appkit/nsview/1483458-initwithframe.json');
     const decl = MethodDeclaration.initFromTokens(
       'fakeid',
       interfaceDecl,
-      initWithFrame.primaryContentSections[0].declarations[0].tokens
+      method.primaryContentSections[0].declarations[0].tokens
+    );
+    expect(decl.generate()).toMatchSnapshot();
+  });
+
+  it('multiple params', () => {
+    const method = require('../documentation/appkit/nsview/1483640-addsubview.json');
+    const decl = MethodDeclaration.initFromTokens(
+      'fakeid',
+      interfaceDecl,
+      method.primaryContentSections[0].declarations[0].tokens
     );
     expect(decl.generate()).toMatchSnapshot();
   });
